0b7827a28c1a5433d44e1221884b8a6214cb3f9e
Azril Rojaan Fauzi <endogcity@gmail.com>
2024-08-28 08:07:30 -0600

    Pengerjaan day 3

--- js/script.js
+++ js/script.js
@@ -16,11 +16,11 @@ for (i = 0; i < genderElement.length; i++) {
       genderValue = genderElement[i].value;
 }
 
-
+const date = new Date();
 document.querySelector('.second-form').innerHTML = `
   <form class="second-form">
         <div class="second-group-form js-second-group-form">
-          <p>Current Time : Fri Jun 17 2022</p>
+          <p>Current Time : ${date}</p>
           <p> </p>
           <p>Nama : ${namaValue}</p>
           <p>Tanggal lahir : ${tanggalValue}</p>
@@ -30,3 +30,34 @@ document.querySelector('.second-form').innerHTML = `
       </form>
 `
 }
+
+function deleteButton(){
+  document.querySelector('.main-form').innerHTML = `
+    <form class="main-form">
+        <div class="main-group">
+        <div class="form-group">
+          <label for="nama">Nama </label>
+          <input type="text" id="nama" name="nama" placeholder="Nama" class="js-input-nama">
+      </div>
+      <div class="form-group">
+          <label for="tanggal-lahir">Tanggal Lahir </label>
+          <input type="date" id="tanggal-lahir" name="tanggal-lahir" class="js-input-tanggal" >
+      </div>
+      <div class="form-group">
+          <label>Jenis Kelamin </label>
+              <label><input type="radio" name="gender" value="Laki-Laki"> Laki-Laki</label>
+              <label><input type="radio" name="gender" value="Perempuan"> Perempuan</label>
+      </div>
+      <div class="form-group">
+          <label for="pesan">Pesan </label>
+          <textarea id="pesan" name="pesan" placeholder="Pesan" class="js-input-pesan"></textarea>
+      </div>
+      </form>
+  `
+}
+
+function replaceName(){
+  let name = prompt('Masukkan nama anda :','')
+  document.querySelector('.name').innerHTML=name;
+}
+replaceName();
